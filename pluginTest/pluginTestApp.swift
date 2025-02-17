4ce1e46b098cd35cf25f7956dc6a74650b6d31e6
sTunahanÜ <tunahan.ustuntepe@securifyidentity.com>
2025-02-17 18:29:02 +0300

    Initial Commit

---
+++ pluginTest/pluginTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  pluginTestApp.swift
+//  pluginTest
+//
+//  Created by Securify on 17.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct pluginTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
